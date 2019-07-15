d0d76628ee5de77a1668f96b193f5f67cc35e40a
Shounak Datta <shounakdatta@Shounaks-MacBook-Pro.local>
2019-07-15 09:33:04 -0400

    Added gitignore

--- index.js
+++ index.js
@@ -1,37 +1,24 @@
-// require('dotenv').config();
-// const { App } = require('@slack/bolt');
-const http = require('http')
+require("dotenv").config();
+const { App } = require("@slack/bolt");
 
 // Initializes your app with your bot token and signing secret
-// const app = new App({
-//     token: process.env.SLACK_BOT_TOKEN,
-//     signingSecret: process.env.SLACK_SIGNING_SECRET
-// });
-
-// // Listens to incoming messages that contain "hello"
-// app.message('hello', ({ message, say }) => {
-//     // say() sends a message to the channel where the event was triggered
-//     say(`Hey there <@${message.user}>!`);
-// });
-
-// (async () => {
-//     // Start your app
-//     await app.start(process.env.PORT || 4390);
-
-//     console.log('⚡️ Bolt app is running!');
-// })();
-
-const PORT = 4390;
-
-function handleRequest(request, response) {
-    console.log(request);
-
-    response.end(request);
-}
-
-var server = http.createServer(handleRequest);
-
-server.listen(PORT, function () {
-    console.log("Server listening on: http://localhost:%s", PORT);
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
+// Listens to incoming messages that contain "hello"
+app.message(
+  ({ message, next }) => next(),
+  ({ message, say }) => {
+    // say() sends a message to the channel where the event was triggered
+    say(`Hey there <@${message.user}>!`);
+  }
+);
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 4390);
+
+  console.log("⚡️ Bolt app is running!");
+})();
